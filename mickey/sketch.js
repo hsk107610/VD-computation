41fe988daae943ff10f2d448688f2c20033b69a7
hsk1075 <hsk1075@snu.ac.kr>
2025-09-16 19:39:22 +0900

    first commit

---
+++ mickey/sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+
+  createCanvas(600, 600);
+  background(200);
+  noStroke();
+}
+
+function draw() {
+ background(mouseX/2,35);
+
+  //face
+  fill(255, 200, 0);
+  ellipse(300, 300, 300, 400);
+
+  //hat
+  fill(90, 50, 20);
+  ellipse(300,120, 190,90);
+
+  //ear
+  fill(255, 200, 0);
+   ellipse(150, 250, 90, 190);
+
+   //ear
+  fill(255, 200, 0);
+  ellipse(450, 250, 90, 190);
+
+  //eyes
+  fill(0,0,0);
+  circle(250,200,20);
+
+  fill(0,0,0);
+  circle(350,200,20);
+
+  //nose
+  fill(0,0,0);
+  ellipse(300,220,50, 30);
+
+  foot
+}
+
+
