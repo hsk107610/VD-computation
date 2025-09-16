2b4ffa8d976a761207e0048f75660b940061dfa7
hsk1075 <hsk1075@snu.ac.kr>
2025-09-16 20:40:47 +0900

    20:40

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -1,11 +1,15 @@
-function preload(){
+let body;
+let face;
+
+function preload() {
+  body = loadImage('2.png');
   face = loadImage('1.png');
 }
 
 function setup() {
   createCanvas(400, 400);
-  background(220); 
-  
-  image(face, 0, 0);
+  background('beige');
+  image(body, 0, 0);
+  image(face, 132, 134);
 }
 
