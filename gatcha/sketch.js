7dbd9182b23cc58f82d51e2f473f8a093ec29eb7
hsk1075 <hsk1075@snu.ac.kr>
2025-09-21 13:22:10 +0900

    gatcha

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -1,15 +1,65 @@
-let body;
+let n;
 let face;
 
 function preload() {
-  body = loadImage('2.png');
-  face = loadImage('1.png');
+    // get random face
+  let n = int(random(1, 10));
+  let imgName = n + ".png";  
+  face = loadImage(imgName);
 }
+
 
 function setup() {
   createCanvas(400, 400);
-  background('beige');
-  image(body, 0, 0);
-  image(face, 132, 134);
+
+  let button = createButton('Hi');
+  button.position(10, 10);
+
+  button.mousePressed(() => {
+    n = int(random(1, 10));
+    let imgName = n + ".png";
+    face = loadImage(imgName);
+  });  
+
+
 }
 
+function draw() {
+  background("beige");
+
+  if( n === 1 ) {
+  background('beige');
+  }
+  if( n === 2 ) {
+  background(0,11,85);
+  }
+  if( n === 3 ) {
+  background('pink');
+  }
+  if( n === 4 ) {
+  background(167, 251, 255);
+  }
+  if( n === 5 ) {
+  background('black');
+  }
+  if( n === 6 ) {
+  background('gray');
+  }
+  if( n === 7 ) {
+  background(255, 251, 148);
+  }
+  if( n === 8 ) {
+  background(255, 0, 200);
+  }
+  if( n === 9 ) {
+  background(148, 134, 255);
+  }
+  if( n === 10 ) {
+   background(48, 29, 0);
+  }
+  image(face, 0, 0);  
+
+
+
+
+}
