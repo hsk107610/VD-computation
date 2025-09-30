1f6de06c5ae4ddacd2bc68909846a2c9b65a2681
hsk1075 <hsk1075@snu.ac.kr>
2025-09-30 15:28:00 +0900

    sun2

--- pattern/sketch.js
+++ pattern/sketch.js
@@ -1,39 +1,38 @@
-const GRID = 9;
+const GRID = 8;
 const CELL = 1080 / GRID;
 
+
 function setup() {
- createCanvas(1080, 1080);
- noStroke();
- frameRate(6);
- //noLoop();
+  createCanvas(1080, 1080);
+  noStroke();
+  frameRate(6);
 }
 
-function draw() {
- background(255, 255, 0);
- const cz = random(0, CELL);
- const sz = random(50, CELL);
+function draw() { background(255, 255, 0); 
+  const cz = random(0, CELL); 
+  const sz = random(70, 100); 
+  for (let gy = 0; gy < GRID; gy++) { 
+    for (let gx = 0; gx < GRID; gx++) { 
+      const cx = gx * CELL + CELL / 2; 
+      const cy = gy * CELL + CELL / 2;
 
- for (let gy = 0; gy < GRID; gy++) {
+      // 원 그리기
+      push();
+      blendMode(OVERLAY);
+      fill('cyan');
+      circle(cx, cy, cz);
+      pop();
 
-   for (let gx = 0; gx < GRID; gx++) {
-     const cx = gx * CELL + CELL / 2;
-     const cy = gy * CELL + CELL / 2;
-
-     push();
-     blendMode(OVERLAY);
-     fill('black');
-     circle(cx, cy, cz);
-     pop();
-
-     push();
-     blendMode(MULTIPLY);
-     translate(cx, cy);
-     fill('pink');
-     rectMode(CENTER);
-     rect(0, 60, sz, sz);
-     pop();
-   }
- }
+      // 사각형 그리기
+      push();
+      blendMode(MULTIPLY);
+      translate(cx, cy);
+      fill('pink');
+      rectMode(CENTER);
+      rect(0, 60, sz, sz, 20);
+      pop();
+    }
+  }
 }
 
 function keyPressed() {
